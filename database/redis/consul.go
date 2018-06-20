b6dc4be3265dff3c4926e8b034f89713af9b30a5
jream <ms.08.lu@gmail.com>
2018-06-20 17:35:23 +0800

    feat(redis): init

--- database/redis/consul.go
+++ database/redis/consul.go
@@ -1,5 +1,47 @@
 package redis
 
+import (
+	"fmt"
+	"path"
+
+	"github.com/JREAMLU/j-core/consul"
+)
+
+// MasterSlave master slave
+type MasterSlave bool
+
+// Configs redis configs in consul
+type Configs struct {
+	InstanceName string
+	Master       []master
+	Slave        []slave
+}
+
+type master struct {
+	DB        string
+	IP        string
+	Port      string
+	PoolSize  int
+	IsCluster bool
+	IsMaster  bool
+}
+
+type slave struct {
+	DB        string
+	IP        string
+	Port      string
+	PoolSize  int
+	IsCluster bool
+	IsMaster  bool
+}
+
+func (masterSlave MasterSlave) String() string {
+	if masterSlave {
+		return "master"
+	}
+	return "slave"
+}
+
 // Load load redis
 func Load(consulAddr string, isWatching bool, names ...string) error {
 	return LoadConfig(consulAddr, isWatching, names...)
@@ -7,5 +49,61 @@ func Load(consulAddr string, isWatching bool, names ...string) error {
 
 // LoadConfig load config
 func LoadConfig(consulAddr string, isWatching bool, names ...string) error {
+	client, err := consul.NewClient(consul.SetAddress(consulAddr))
+	if err != nil {
+		return err
+	}
+
+	if err = loadAll(client); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func loadAll(client *consul.Client) error {
+	// 获取所有实例的key
+	keys, err := client.GetChildKeys(consul.Redis)
+	if err != nil {
+		return err
+	}
+
+	return loadConfig(client, keys)
+}
+
+func loadConfig(client *consul.Client, prefixKeys []string) error {
+	loadMasterOrSlave := func(key, instanceName string, isMaster MasterSlave) error {
+		keys, err := client.GetChildKeys(key)
+		if err != nil {
+			return err
+		}
+
+		if len(keys) == 0 && isMaster {
+			return fmt.Errorf(consul.DirNotExist, key)
+		}
+
+		for i := range keys {
+			if err = loadNode(client, isMaster, keys[i], instanceName); err != nil {
+				return err
+			}
+		}
+
+		return nil
+	}
+
+	for _, key := range prefixKeys {
+		instanceName := path.Base(key)
+		if err := loadMasterOrSlave(key, instanceName, true); err != nil {
+			return err
+		}
+		if err := loadMasterOrSlave(key, instanceName, false); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func loadNode(client *consul.Client, isMaster MasterSlave, keyPre, instanceName string) error {
 	return nil
 }
---
+++ database/redis/consul_test.go
@@ -0,0 +1,21 @@
+package redis
+
+import (
+	"fmt"
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+const (
+	consulAddr = "10.200.202.35:8500"
+)
+
+func TestLoadConfig(t *testing.T) {
+	Convey("load redis test", t, func() {
+		Convey("load all", func() {
+			err := Load(consulAddr, false)
+			fmt.Println("++++++++++++: ", err)
+		})
+	})
+}
