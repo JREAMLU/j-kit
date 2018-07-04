f114fc0cdcb2be2f2cfb33de1feb73192219235c
jream <ms.08.lu@gmail.com>
2018-07-04 15:45:37 +0800

    feat(redis): redis pool

--- database/redis/pool.go
+++ database/redis/pool.go
@@ -28,15 +28,61 @@ func newPool(addr string, maxIdle int, idleTimeout time.Duration) *Pool {
 	}
 }
 
-func (p *Pool) get(db string) (pool *redis.Pool) {
+func (p *Pool) get(db string) (rPool *redis.Pool) {
 	p.rwMutex.RLock()
-	pool = p.pools[db]
+	rPool = p.pools[db]
 	p.rwMutex.RUnlock()
-	return
+
+	return rPool
 }
 
-// GetPool get redis pool
-func GetPool(addr, db string, maxIdle int, idleTimeout time.Duration) *redis.Pool {
-	// @TODO pool
+// Get get or register pool
+func (p *Pool) Get(db string) (rPool *redis.Pool) {
+	rPool = p.get(db)
+	if rPool != nil {
+		return rPool
+	}
+
+	return p.register(db)
+}
+
+// @TODO register pool
+func (p *Pool) register(db string) (rPool *redis.Pool) {
 	return nil
+}
+
+// GetPool get redis pool Ingress
+func GetPool(addr, db string, maxIdle int, idleTimeout time.Duration) *redis.Pool {
+	pool := getPool(addr, db)
+	if pool != nil {
+		return pool
+	}
+
+	return registerPool(addr, maxIdle, idleTimeout).Get(db)
+}
+
+func getPool(addr, db string) (rPool *redis.Pool) {
+	rwMutex.RLock()
+	if pool, ok := pools[addr]; ok {
+		rPool = pool.Get(db)
+	}
+	rwMutex.RUnlock()
+
+	return rPool
+}
+
+func registerPool(addr string, maxIdle int, idleTimeout time.Duration) *Pool {
+	var (
+		pool *Pool
+		ok   bool
+	)
+
+	rwMutex.Lock()
+	if pool, ok = pools[addr]; !ok {
+		pool = newPool(addr, maxIdle, idleTimeout)
+		pools[addr] = pool
+	}
+	rwMutex.Unlock()
+
+	return pool
 }
