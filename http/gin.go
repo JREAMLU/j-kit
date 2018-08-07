d4d47f48619b75ed5b5ba4da322772b6f73117f2
jream <ms.08.lu@gmail.com>
2018-08-07 14:06:11 +0800

    feat(cb): http

--- http/gin.go
+++ http/gin.go
@@ -3,19 +3,23 @@ package http
 import (
 	"log"
 	"os"
+	"time"
 
 	middleware "github.com/JREAMLU/j-kit/go-micro/gin-middleware"
 	jopentracing "github.com/JREAMLU/j-kit/go-micro/trace/opentracing"
 	"github.com/JREAMLU/j-kit/go-micro/util"
+	"github.com/sony/gobreaker"
 
 	"github.com/gin-gonic/gin"
 	"github.com/hashicorp/consul/api"
 	micro "github.com/micro/go-micro"
+	microClient "github.com/micro/go-micro/client"
 	"github.com/micro/go-micro/registry"
 	client "github.com/micro/go-plugins/client/grpc"
 	register "github.com/micro/go-plugins/registry/consul"
 	server "github.com/micro/go-plugins/server/grpc"
 	transport "github.com/micro/go-plugins/transport/grpc"
+	microGobreaker "github.com/micro/go-plugins/wrapper/breaker/gobreaker"
 	opentracing "github.com/opentracing/opentracing-go"
 )
 
@@ -32,7 +36,22 @@ func NewHTTPService(config *util.Config) (micro.Service, *gin.Engine, opentracin
 	}
 
 	service := micro.NewService(
-		micro.Client(client.NewClient()),
+		micro.Client(client.NewClient(
+			microClient.Wrap(microGobreaker.NewClientWrapper(
+				gobreaker.NewCircuitBreaker(gobreaker.Settings{
+					Name:        config.Service.Name,
+					MaxRequests: config.CircuitBreaker.MaxRequests,
+					Interval:    time.Duration(config.CircuitBreaker.Interval) * time.Second,
+					Timeout:     time.Duration(config.CircuitBreaker.Timeout) * time.Second,
+					ReadyToTrip: func(counts gobreaker.Counts) bool {
+						failureRatio := float64(counts.TotalFailures) / float64(counts.Requests)
+						return counts.Requests >= config.CircuitBreaker.CountsRequests && failureRatio >= config.CircuitBreaker.FailureRatio
+					},
+					OnStateChange: func(name string, from gobreaker.State, to gobreaker.State) {
+					},
+				}),
+			)),
+		)),
 		micro.Server(server.NewServer()),
 		micro.Registry(register.NewRegistry(
 			registry.Option(func(opts *registry.Options) {
